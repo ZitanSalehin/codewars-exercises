23509a74538d82047a3f6df1d158d2d066617cda
Zitan Salehin <zitan-74@dipti.com.bd>
2024-04-14 09:11:14 +0600

    some solutions

--- fundamentals/functionPassingParams.js
+++ fundamentals/functionPassingParams.js
@@ -6,6 +6,7 @@
 // name equals owner	'Hello boss'
 // otherwise	'Hello guest'
 
+// 1st ans:
 const greetingMsg=(guestName,owner)=>{
     if(guestName===owner) return 'Hello boss'
     else return 'Hello guest'
@@ -13,3 +14,16 @@ const greetingMsg=(guestName,owner)=>{
 var guestName;
 var owner; 
 greetingMsg(guestName,owner);
+
+// 2nd ans:
+const greetingsMsg = (guestNames, owners) => (guestNames === owners) ? 'Hello boss' : 'Hello guest';
+
+// 3rd ans:
+function greet (name, owner) {
+  return name === owner ? 'Hello boss' : 'Hello guest';
+}
+
+// 4th ans:
+function greet (name, owner) {
+  return `Hello ${name==owner?'boss':'guest'}`
+}
